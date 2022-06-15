7c437682dd84640f8306c935265405bdb2d04afe
Leifer Mendez <leifer33@gmail.com>
2022-06-15 09:46:33 +0200

    update package

--- dist/data/data-source.js
+++ dist/data/data-source.js
@@ -1,15 +1,20 @@
 "use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.AppDataSource = void 0;
+const dotenv_1 = __importDefault(require("dotenv"));
 const typeorm_1 = require("typeorm");
 const mail_entity_1 = require("../entity/mail.entity");
+dotenv_1.default.config();
 exports.AppDataSource = new typeorm_1.DataSource({
     type: "mysql",
     host: "localhost",
     port: 3306,
-    username: "root",
-    password: "",
-    database: "webpublica",
+    username: process.env.DATABASE_USER,
+    password: process.env.DATABASE_PASS,
+    database: process.env.DATABASE_NAME,
     synchronize: true,
     logging: false,
     entities: [mail_entity_1.Mail],
