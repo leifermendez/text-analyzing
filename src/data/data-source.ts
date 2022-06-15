8636729ad1995ddbf2efd72803a9946f666f0fd3
Leifer Mendez <leifer33@gmail.com>
2022-06-15 09:45:53 +0200

    update package

--- src/data/data-source.ts
+++ src/data/data-source.ts
@@ -1,16 +1,18 @@
+import dotenv from "dotenv";
 import { DataSource } from "typeorm";
 import { Mail } from "../entity/mail.entity";
+dotenv.config();
 
 export const AppDataSource = new DataSource({
-    type: "mysql",
-    host: "localhost",
-    port: 3306,
-    username: "root",
-    password: "",
-    database: "webpublica",
-    synchronize: true,
-    logging: false,
-    entities: [Mail],
-    subscribers: [],
-    migrations: [],
-})
+  type: "mysql",
+  host: "localhost",
+  port: 3306,
+  username: process.env.DATABASE_USER,
+  password: process.env.DATABASE_PASS,
+  database: process.env.DATABASE_NAME,
+  synchronize: true,
+  logging: false,
+  entities: [Mail],
+  subscribers: [],
+  migrations: [],
+});
